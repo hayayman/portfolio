38b6a18d206ff7aee3f8dc246de81acfe9749a85
ayman <ayman.imam200@gmail.com>
2024-01-09 21:12:56 +0100

    first commit to create my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <>
+    
+      <h1>list of projects</h1>
+
+      <ul>
+        <li><h3><a href="https://hayayman.github.io/root/">firt deploy</a></h3></li>
+        <li><h3><a href="https://hayayman.github.io/hayst/">firt deploy</a></h3></li>
+      </ul>
+
+      <hr />
+
+      <h2>
+         &copy; 
+          <a href="">that was my port folio</a>
+      </h2>
+
+    </>
+  );
+}
+
+export default App;
